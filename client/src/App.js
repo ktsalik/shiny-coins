597cdf6e31fcd58bdd24ca90c40093bb64cbcdaf
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-03-31 13:20:53 +0300

    change app default route to be more friendly

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import {
   BrowserRouter,
+  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
@@ -25,7 +26,8 @@ function App() {
       {JSON.stringify(apiTestResponse)}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Coins></Coins>}></Route>
+          <Route path="/" element={<Navigate to="/coins" replace={true} />}></Route>
+          <Route path="/coins" element={<Coins />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
