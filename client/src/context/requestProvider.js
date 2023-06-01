8585c25fe1f9319cdf9d359f0b5ffd608c49b88a
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-06-01 07:37:05 +0300

    change api url on client

--- client/src/context/requestProvider.js
+++ client/src/context/requestProvider.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createContext } from "react";
 
 const instance = axios.create({
-  baseURL: 'https://shinycoins.cyclic.app/api/',
+  baseURL: 'https://repulsive-fox-visor.cyclic.app/api/',
 });
 
 export const requestInstance = instance;
